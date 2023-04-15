b83d6f21f5403ba08d44354aff852b21bf5c27a5
adityagoel28 <adityagoel2002@gmail.com>
2023-04-16 00:09:44 +0530

    featured updated

--- ReactWebsite/src/App.js
+++ ReactWebsite/src/App.js
@@ -23,7 +23,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import Community from './components/Community';
 import Modal from './components/Modal';
 import Soil from './components/Soil';
-import Crop from './components/Crop';
 import Location from './components/Location';
 import Schemes from './components/Schemes';
 import CropSuggest from './components/CropSuggest';
